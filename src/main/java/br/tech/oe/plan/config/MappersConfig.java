da619420e43b155f6fb949386ece14932baff534
Lucas Josino <contact@lucasjosino.com>
2025-05-07 17:41:08 -0300

    feat: add ModelMapper dependency and configuration

--- src/main/java/br/tech/oe/plan/config/MappersConfig.java
+++ src/main/java/br/tech/oe/plan/config/MappersConfig.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.convention.NameTokenizers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +23,17 @@ public class MappersConfig {
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setPropertyCondition(Conditions.isNotNull())
+                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
+        return modelMapper;
     }
 }
