16e9ea86e24af64397a1042ba0190fd4355ac2c1
Lucas Josino <contact@lucasjosino.com>
2025-05-08 10:05:33 -0300

    fix: wrong APIController mapping

--- src/main/java/br/tech/oe/plan/controller/v1/APIController.java
+++ src/main/java/br/tech/oe/plan/controller/v1/APIController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/api")
+@RequestMapping("/api/v1/")
 public class APIController {
 
     private final APIInfoDTO apiInfo;
