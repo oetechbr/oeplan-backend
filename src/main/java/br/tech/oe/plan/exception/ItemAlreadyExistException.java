388927a16257eddb14787dfcfe6c0ac3adef3161
Lucas Josino <contact@lucasjosino.com>
2025-05-08 20:40:45 -0300

    feat: add custom exceptions

---
+++ src/main/java/br/tech/oe/plan/exception/ItemAlreadyExistException.java
@@ -0,0 +1,19 @@
+package br.tech.oe.plan.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class ItemAlreadyExistException extends RuntimeException {
+
+    public ItemAlreadyExistException() {
+    }
+
+    public ItemAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public ItemAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
