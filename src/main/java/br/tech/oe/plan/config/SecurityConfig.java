3f5568cb191f5030ec1fa04d344a1f3c9e30bcba
Lucas Josino <contact@lucasjosino.com>
2025-05-08 20:40:01 -0300

    feat: add spring configuration

--- src/main/java/br/tech/oe/plan/config/SecurityConfig.java
+++ src/main/java/br/tech/oe/plan/config/SecurityConfig.java
@@ -1,19 +1,74 @@
 package br.tech.oe.plan.config;
 
+import br.tech.oe.plan.service.impl.AuthServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${server.servlet.session.cookie.name}")
+    private String sessionCookieName;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                        .requestMatchers(HttpMethod.GET, "/api/v1/ping").hasAnyRole("ADMIN")
                         .anyRequest().permitAll()
-                ).build();
+                )
+                .logout(logout -> logout
+                        .deleteCookies(sessionCookieName)
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                )
+                .securityContext((context) -> context
+                        .securityContextRepository(httpSessionSecurityContextRepository())
+                        .requireExplicitSave(true)
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthServiceImpl authService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(authService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public HttpSessionSecurityContextRepository httpSessionSecurityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
