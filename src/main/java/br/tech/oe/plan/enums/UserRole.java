3226dea27c7c95fd3780552e2c8613a16144c4c7
Lucas Josino <contact@lucasjosino.com>
2025-05-09 22:35:21 -0300

    fix: add missing user role character

--- src/main/java/br/tech/oe/plan/enums/UserRole.java
+++ src/main/java/br/tech/oe/plan/enums/UserRole.java
@@ -3,6 +3,7 @@ package br.tech.oe.plan.enums;
 public enum UserRole {
     ADMIN("admin"),
     DIRECTOR("director"),
+    COORDINATOR("coordinator"),
     TEACHER("teacher");
 
     UserRole(String role) {
