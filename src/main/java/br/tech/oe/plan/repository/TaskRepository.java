70f4077d1ffcca085592598258f66e02926d8c73
Lucas Josino <contact@lucasjosino.com>
2025-05-12 10:05:20 -0300

    fix: n+1 problem with task model

--- src/main/java/br/tech/oe/plan/repository/TaskRepository.java
+++ src/main/java/br/tech/oe/plan/repository/TaskRepository.java
@@ -1,11 +1,17 @@
 package br.tech.oe.plan.repository;
 
 import br.tech.oe.plan.model.TaskModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    @Override
+    @EntityGraph(attributePaths = {"status", "priority", "visibility", "assignedBy", "assignedTo"})
+    List<TaskModel> findAll();
 }
