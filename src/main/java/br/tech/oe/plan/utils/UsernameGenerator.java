5526214097a8e0e5ad0de7fd4956bad3d70c1063
Lucas Josino <contact@lucasjosino.com>
2025-05-14 16:03:27 -0300

    chore: remove unused file

--- src/main/java/br/tech/oe/plan/utils/UsernameGenerator.java
+++
@@ -1,9 +0,0 @@
-package br.tech.oe.plan.utils;
-
-public class UsernameGenerator {
-
-    public static String fromFullName(String firstName, String lastName) {
-        if (firstName == null || lastName == null) return null;
-        return (firstName + "-" + lastName).toLowerCase().replaceAll("[^a-z0-9.]", "");
-    }
-}
