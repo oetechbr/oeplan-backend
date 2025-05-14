e80193478ede7b5799fedca561c87aeed6bc4514
Lucas Josino <contact@lucasjosino.com>
2025-05-14 10:03:36 -0300

    wip: add initial task POST and PATCH methods

--- src/main/java/br/tech/oe/plan/controller/v1/TaskController.java
+++ src/main/java/br/tech/oe/plan/controller/v1/TaskController.java
@@ -4,13 +4,16 @@ import br.tech.oe.plan.controller.v1.interfaces.BaseController;
 import br.tech.oe.plan.controller.v1.interfaces.CommentController;
 import br.tech.oe.plan.dto.comment.CommentDTO;
 import br.tech.oe.plan.dto.comment.CreateCommentDTO;
+import br.tech.oe.plan.dto.task.CreateTaskDTO;
 import br.tech.oe.plan.dto.task.TaskDTO;
+import br.tech.oe.plan.dto.task.UpdateTaskDTO;
 import br.tech.oe.plan.service.CommentService;
 import br.tech.oe.plan.service.TaskService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -36,6 +39,16 @@ public class TaskController implements BaseController<TaskDTO>, CommentControlle
     @Override
     public ResponseEntity<TaskDTO> findById(UUID uuid) {
         return ResponseEntity.ok(taskService.findById(uuid));
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDTO> save(@RequestBody @Valid CreateTaskDTO task) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.save(task));
+    }
+
+    @PatchMapping("/{uuid}")
+    public ResponseEntity<TaskDTO> save(@PathVariable UUID uuid, @RequestBody @Valid UpdateTaskDTO task) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.patch(uuid, task));
     }
 
     @Override
--- src/main/java/br/tech/oe/plan/service/TaskService.java
+++ src/main/java/br/tech/oe/plan/service/TaskService.java
@@ -1,6 +1,13 @@
 package br.tech.oe.plan.service;
 
+import br.tech.oe.plan.dto.task.CreateTaskDTO;
 import br.tech.oe.plan.dto.task.TaskDTO;
+import br.tech.oe.plan.dto.task.UpdateTaskDTO;
+
+import java.util.UUID;
 
 public interface TaskService extends BaseService<TaskDTO> {
+    TaskDTO save(CreateTaskDTO task);
+
+    TaskDTO patch(UUID uuid, UpdateTaskDTO task);
 }
