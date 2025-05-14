0f0412c99331c3fa339a195e721723f40afbdfe8
Lucas Josino <contact@lucasjosino.com>
2025-05-14 12:49:58 -0300

    refactor: remove BaseController

--- src/main/java/br/tech/oe/plan/controller/v1/GroupController.java
+++ src/main/java/br/tech/oe/plan/controller/v1/GroupController.java
@@ -1,9 +1,9 @@
 package br.tech.oe.plan.controller.v1;
 
-import br.tech.oe.plan.controller.v1.interfaces.BaseController;
 import br.tech.oe.plan.dto.group.GroupDTO;
 import br.tech.oe.plan.service.GroupService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/groups")
-public class GroupController implements BaseController<GroupDTO> {
+public class GroupController {
 
     private final GroupService groupService;
 
@@ -20,12 +20,12 @@ public class GroupController implements BaseController<GroupDTO> {
         this.groupService = groupService;
     }
 
-    @Override
+    @GetMapping
     public ResponseEntity<List<GroupDTO>> findAll() {
         return ResponseEntity.ok(groupService.findAll());
     }
 
-    @Override
+    @GetMapping("/{uuid}")
     public ResponseEntity<GroupDTO> findById(UUID uuid) {
         return ResponseEntity.ok(groupService.findById(uuid));
     }
--- src/main/java/br/tech/oe/plan/controller/v1/TaskController.java
+++ src/main/java/br/tech/oe/plan/controller/v1/TaskController.java
@@ -1,22 +1,20 @@
 package br.tech.oe.plan.controller.v1;
 
-import br.tech.oe.plan.controller.v1.interfaces.BaseController;
-import br.tech.oe.plan.controller.v1.interfaces.CommentController;
 import br.tech.oe.plan.dto.comment.CommentDTO;
 import br.tech.oe.plan.dto.comment.CreateCommentDTO;
 import br.tech.oe.plan.dto.task.TaskDTO;
 import br.tech.oe.plan.service.CommentService;
 import br.tech.oe.plan.service.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
-public class TaskController implements BaseController<TaskDTO>, CommentController {
+public class TaskController {
 
     private final TaskService taskService;
 
@@ -27,32 +25,35 @@ public class TaskController implements BaseController<TaskDTO>, CommentControlle
         this.commentService = commentService;
     }
 
-    @Override
+    @GetMapping
     public ResponseEntity<List<TaskDTO>> findAll() {
         return ResponseEntity.ok(taskService.findAll());
     }
 
-    @Override
-    public ResponseEntity<TaskDTO> findById(UUID uuid) {
+    @GetMapping("/{uuid}")
+    public ResponseEntity<TaskDTO> findById(@PathVariable UUID uuid) {
         return ResponseEntity.ok(taskService.findById(uuid));
     }
 
-    @Override
-    public ResponseEntity<List<CommentDTO>> findAllComments(UUID taskUuid) {
+    @GetMapping("/{taskUuid}/comments")
+    public ResponseEntity<List<CommentDTO>> findAllComments(@PathVariable UUID taskUuid) {
         return ResponseEntity.ok(commentService.findAll(taskUuid));
     }
 
-    @Override
-    public ResponseEntity<CommentDTO> findCommentById(UUID taskUuid, UUID uuid) {
+    @GetMapping("/{taskUuid}/comments/{uuid}")
+    public ResponseEntity<CommentDTO> findCommentById(@PathVariable UUID taskUuid, @PathVariable UUID uuid) {
         return ResponseEntity.ok(commentService.findById(taskUuid, uuid));
     }
 
-    @Override
-    public ResponseEntity<CommentDTO> saveComment(CreateCommentDTO commentDTO, UUID taskUuid) {
-        return ResponseEntity.ok(commentService.save(commentDTO, taskUuid));
+    @PostMapping("/{taskUuid}/comments")
+    public ResponseEntity<CommentDTO> saveComment(
+            @PathVariable UUID taskUuid,
+            @RequestBody @Valid CreateCommentDTO commentDTO
+    ) {
+        return ResponseEntity.ok(commentService.save(taskUuid, commentDTO));
     }
 
-    @Override
+    @DeleteMapping("/{taskUuid}/comments/{uuid}")
     public ResponseEntity<Void> deleteComment(UUID uuid) {
         commentService.delete(uuid);
         return ResponseEntity.noContent().build();
