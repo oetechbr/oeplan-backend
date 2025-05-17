237cca0448a28a27a99f93166623f4629a8cea24
Lucas Josino <contact@lucasjosino.com>
2025-05-17 10:31:01 -0300

    feat: add SimpleUserDTO

---
+++ src/main/java/br/tech/oe/plan/dto/SimpleUserDTO.java
@@ -0,0 +1,64 @@
+package br.tech.oe.plan.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SimpleUserDTO implements Serializable {
+    private UUID uuid;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String avatarUrl;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
