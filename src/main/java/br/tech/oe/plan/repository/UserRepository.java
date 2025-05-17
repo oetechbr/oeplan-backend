d9545ff365cca8ca2c46e2bca263a169b0fa886f
Lucas Josino <contact@lucasjosino.com>
2025-05-17 10:33:05 -0300

    feat: add findByEmail to UserRepository

--- src/main/java/br/tech/oe/plan/repository/UserRepository.java
+++ src/main/java/br/tech/oe/plan/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package br.tech.oe.plan.repository;
 
 import br.tech.oe.plan.model.UserModel;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
-
-    @EntityGraph(attributePaths = {"role", "status"})
     UserModel findByUsername(String username);
+
+    Optional<UserModel> findByEmail(String email);
 }
