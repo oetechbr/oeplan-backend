c205482bce0d9732112f7a60b6d4e6c583734cd0
Lucas Josino <contact@lucasjosino.com>
2025-06-02 22:39:11 -0300

    refactor: add more fields to CreateUserDTO

--- src/main/java/br/tech/oe/plan/dto/user/CreateUserDTO.java
+++ src/main/java/br/tech/oe/plan/dto/user/CreateUserDTO.java
@@ -1,12 +1,14 @@
 package br.tech.oe.plan.dto.user;
 
 import br.tech.oe.plan.enums.UserRole;
+import br.tech.oe.plan.enums.UserStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateUserDTO implements Serializable {
@@ -25,6 +27,20 @@ public class CreateUserDTO implements Serializable {
 
     @NotNull
     private UserRole role;
+
+    private UserStatus status = UserStatus.INVITED;
+
+    private String phone;
+
+    private String gender;
+
+    private LocalDateTime birthDate;
+
+    @NotNull
+    private String department;
+
+    @NotNull
+    private String titlePosition;
 
     public String getUsername() {
         return username;
@@ -64,5 +80,53 @@ public class CreateUserDTO implements Serializable {
 
     public void setRole(UserRole role) {
         this.role = role;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getTitlePosition() {
+        return titlePosition;
+    }
+
+    public void setTitlePosition(String titlePosition) {
+        this.titlePosition = titlePosition;
     }
 }
